e0ee2003a41412b247e45c87f91560e71e7a22d6
Maxime Eglem <maxime.eglem@edenred.com>
2019-03-06 13:32:48 +0100

    Create Class for managing states

---
+++ Plan/App.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using MapKit;
+
+namespace Plan
+{
+    public static class App
+    {
+        public static string UrlScheme(this AppEnum app)
+        {
+            throw new ModelNotImplementedException();
+        }
+
+        public static string Name(AppEnum app)
+        {
+            throw new ModelNotImplementedException();
+        }
+
+        public static bool Supports(this AppEnum app, ModeEnum mode)
+        {
+            throw new ModelNotImplementedException();
+        }
+
+        public static MKDirectionsMode IdentifierApple(this AppEnum app, ModeEnum mode)
+        {
+            throw new ModelNotImplementedException();
+        }
+
+        public static string Identifier(this AppEnum app, ModeEnum mode)
+        {
+            throw new ModelNotImplementedException();
+        }
+    }
+}
