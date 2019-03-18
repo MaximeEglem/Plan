a740aef973e8ef60dd641210298bde27ee8fdceb
Maxime Eglem <maxime.eglem@edenred.com>
2019-03-18 13:49:10 +0100

    Add Method to check if one app is installed or not

---
+++ Plan/Plan.cs
@@ -0,0 +1,20 @@
+﻿using UIKit;
+
+namespace Plan
+{
+    public static class Plan
+    {
+        public static bool IsInstalled(this AppEnum app)
+        {
+            if (AppEnum.AppleMaps == app)
+                return true;
+
+            var urlScheme = app.UrlScheme();
+
+            if (urlScheme == null)
+                return false;
+
+            return UIApplication.SharedApplication.CanOpenUrl(new Foundation.NSUrl(urlScheme));
+        }
+    }
+}
